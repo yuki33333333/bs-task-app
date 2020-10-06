2e044fe09ccc2ac738b15442ba7be575de078f4c
yuki33333333 <piya.yuki.0828@gmail.com>
2020-10-06 15:22:18 +0900

    new, edit画面雛形作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,37 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+  
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+        redirect_to @task
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update_attributes(task_params)
+      flash[:success] = "Task updated"
+      redirect_to @task
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:name, :description)
+    end
 end
