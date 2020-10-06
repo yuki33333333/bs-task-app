0df55bd51ab718c7a05d0550bb5e505da81f1ca2
yuki33333333 <piya.yuki.0828@gmail.com>
2020-10-06 18:08:53 +0900

    Revert "タスク編集後、リダイレクト先をindexに変更"
    This reverts commit 53e6ac2e8ab17235b9d49e5bfb174081dadaa814.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,7 +29,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     if @task.update_attributes(task_params)
       flash[:success] = "Task updated"
-      redirect_to tasks_path
+      redirect_to @task
     else
       render 'edit'
     end
