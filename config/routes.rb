9b68e8f103c8142986e31c20f1caa1a5ffa8d8b3
yuki33333333 <piya.yuki.0828@gmail.com>
2020-10-06 10:41:11 +0900

    index, showを表示させる

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
+  resources :tasks, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
