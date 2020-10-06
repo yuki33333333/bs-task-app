319c2474e37f9920ab7d8536545f44c4d84cdc08
yuki33333333 <piya.yuki.0828@gmail.com>
2020-10-06 15:20:33 +0900

    ルーティング書き換え

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :tasks, only: [:index, :show]
+  resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
