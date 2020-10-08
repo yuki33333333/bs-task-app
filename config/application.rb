802256d4f88ef75d414e0704bdacb195342b119d
yuki33333333 <piya.yuki.0828@gmail.com>
2020-10-08 17:12:32 +0900

    日本語化

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module App
     # the framework and any gems in your application.
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
 
     # Don't generate system test files.
     config.generators.system_tests = nil
