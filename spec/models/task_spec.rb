9874477888b4c04a4e38e0362801d958585d16d6
yuki33333333 <piya.yuki.0828@gmail.com>
2020-10-13 16:36:29 +0900

    正常系のモデルスペック

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,9 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it "タスク名のバリデーションが有効かどうか" do
-    task = Task.new()
-    task.valid?
-    expect(task.errors.messages[:name]).to include('を入力してください')
+
+  describe 'バリデーションが有効かどうか' do
+    it "is invalid without name column" do
+      task = Task.new()
+      task.valid?
+      expect(task.errors.messages[:name]).to include('を入力してください')
+    end
+  end
+
+  describe 'タスク登録ができるかどうか' do
+
+    it "is valid with name colmun" do
+      task = Task.new(name: "掃除")
+      expect(task).to be_valid
+    end
+
+    it "is valid with name, describe colmun" do
+      task = Task.new(name: "掃除", description: "掃除")
+      expect(task).to be_valid
+    end
+
   end
 end
