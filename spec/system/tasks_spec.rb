817092ca2429ab4a2cf999becff12c45c967b675
yuki33333333 <piya.yuki.0828@gmail.com>
2020-10-19 19:46:01 +0900

    タスクのソートについて System Spec修正

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -6,14 +6,25 @@ RSpec.describe 'Tasks', type: :system do
     let!(:task2) { create(:task, name: 'task2', description: 'task2', created_at: Time.current + 1.days) }
     let!(:task3) { create(:task, name: 'task3', description: 'task3', created_at: Time.current + 2.days) }
 
-    it 'タスク作成日時の降順であること' do
-      # Task一覧画面を開く
-      visit tasks_path
+    context 'タスク作成日時の降順であること' do
+      it do
+        visit tasks_path
+        within '.tasks' do
+          task_names = all('.task-name').map(&:text)
+          expect(task_names).to eq %w(task3 task2 task1)
+        end
+      end
+    end
 
-      # Taskの並び順が新規作成日時順が新しい順であること（task３、２、１の順）
-      within '.tasks' do
-        task_names = all('.task-name').map(&:text)
-        expect(task_names).to eq %w(task3 task2 task1)
+    context '終了期限が古い順であること' do
+      it do
+        visit tasks_path
+        select '終了期限が古い順'
+        click_on '並べ替え'
+        within '.tasks' do
+          task_names = all('.task-name').map(&:text)
+          expect(task_names).to eq %w(task1 task2 task3)
+        end
       end
     end
   end
