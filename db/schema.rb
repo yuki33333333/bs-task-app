64cdea0b798316cb5d75fabb1e68ddb823a7cfeb
本田 幸実 <1112264@PC0628.local>
2020-10-28 12:33:09 +0900

    ユーザーモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_062348) do
+ActiveRecord::Schema.define(version: 2020_10_28_031525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2020_10_21_062348) do
     t.datetime "updated_at", null: false
     t.date "limit"
     t.integer "status"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
