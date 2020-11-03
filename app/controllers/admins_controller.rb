ada5767c1a408b511747924e833e77da4e6986d1
本田 幸実 <1112264@PC0628.local>
2020-11-03 18:00:19 +0900

    adminページの表示・非adminには非表示にする

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,13 @@
 class AdminsController < ApplicationController
-  def show
+  def top
+    current_admin
     @users = User.all
+  end
+
+  private
+
+  def current_admin
+    remember_token = Admin.encrypt(cookies[:remember_token])
+    @current_admin ||= Admin.find_by(remember_token: remember_token)
   end
 end
