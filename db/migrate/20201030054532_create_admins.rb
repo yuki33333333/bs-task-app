42295bc7e25735196d1f6c22f64ced5fc0732281
本田 幸実 <1112264@PC0628.local>
2020-11-04 17:02:27 +0900

    Adminモデルのカラム変更

--- db/migrate/20201030054532_create_admins.rb
+++ db/migrate/20201030054532_create_admins.rb
@@ -4,7 +4,6 @@ class CreateAdmins < ActiveRecord::Migration[5.2]
       t.string :name, null: false
       t.string :mail, null: false
       t.string :password_digest, null: false
-      t.string :remember_token
 
       t.timestamps
     end
