463f321cc391ccd64fc676b570ecc225ef5609c2
本田 幸実 <1112264@PC0628.local>
2020-11-09 15:33:05 +0900

    ユーザーモデルにpasswordを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_30_054532) do
+ActiveRecord::Schema.define(version: 2020_11_09_061427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,10 +35,11 @@ ActiveRecord::Schema.define(version: 2020_10_30_054532) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "tasks", "users"
